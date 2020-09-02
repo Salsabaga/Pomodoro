b8cbd1833c201249044821c7eff1c76b22053af8
Danny Baldeon Abril <dbaldeonabril@gmail.com>
2020-09-02 20:50:03 +0100

    Clock now starts on click, added stop and reset button

--- clock.js
+++ clock.js
@@ -1,16 +1,41 @@
 //Set the start time in minutes
 //followed by the seconds of the startingMinute to get a complete second countdown
+//var gives an interval to use for the clearTimeout function.
 
 let startingMinute = 25;
 let time = startingMinute * 60;
+var timerAction;
 
 //Set our countdown timer on the screen
 
 const countDownText = document.getElementById("countdown");
+const countdownTitle = document.getElementById("title");
+const startButton = document.getElementById("start");
+const resetButton = document.getElementById("reset");
+let isStop = false;
 
 //using our updateCounter function, it will perform the action every second
 
-setInterval(updateCounter, 1000);
+function timerStart() {
+	timerAction = setInterval(updateCounter, 1000);
+	isStop = false;
+}
+
+function timerStop() {
+	if (!isStop) {
+		clearTimeout(timerAction);
+		isStop = true;
+	}
+}
+
+//Reset time by reassigning the time variable to its initial value by multiplying the initial starting time.
+//Then update the value by calling the updateCounter function to update the time again.
+
+function timerReset() {
+	clearTimeout(timerAction);
+	time = startingMinute * 60;
+	updateCounter();
+}
 
 //minutes goes down every minute as its dividing by the total time of the 25 minutes
 //the Math.floor function will round it down
@@ -26,6 +51,8 @@ function updateCounter() {
 		seconds;
 	}
 	countDownText.innerHTML = minutes + ":" + seconds;
-
 	time--;
+	if (time < 0) {
+		countDownText.innerHTML = "Complete!";
+	}
 }
