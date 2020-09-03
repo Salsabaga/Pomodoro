12cf566de707c9206fe1aecb4d7d36b89e699a08
Danny Baldeon Abril <dbaldeonabril@gmail.com>
2020-09-03 13:35:54 +0100

    Add styles and 5 min timer after timer reaches 25

--- clock.js
+++ clock.js
@@ -4,6 +4,7 @@
 
 let startingMinute = 25;
 let time = startingMinute * 60;
+let breakTime = 5;
 var timerAction;
 
 //Set our countdown timer on the screen
@@ -11,7 +12,6 @@ var timerAction;
 const countDownText = document.getElementById("countdown");
 const countdownTitle = document.getElementById("title");
 const startButton = document.getElementById("start");
-const resetButton = document.getElementById("reset");
 let isStop = false;
 
 //using our updateCounter function, it will perform the action every second
@@ -19,12 +19,16 @@ let isStop = false;
 function timerStart() {
 	timerAction = setInterval(updateCounter, 1000);
 	isStop = false;
+	startButton.style.transitionDuration = "all 2s";
+	startButton.style.visibility = "hidden";
 }
 
 function timerStop() {
 	if (!isStop) {
 		clearTimeout(timerAction);
 		isStop = true;
+		startButton.style.visibility = "visible";
+		startButton.innerText = "Resume";
 	}
 }
 
@@ -35,6 +39,7 @@ function timerReset() {
 	clearTimeout(timerAction);
 	time = startingMinute * 60;
 	updateCounter();
+	startButton.innerText = "Start";
 }
 
 //minutes goes down every minute as its dividing by the total time of the 25 minutes
@@ -51,8 +56,19 @@ function updateCounter() {
 		seconds;
 	}
 	countDownText.innerHTML = minutes + ":" + seconds;
+
 	time--;
 	if (time < 0) {
-		countDownText.innerHTML = "Complete!";
+		breakTimer();
+		countdownTitle.innerText = "Well Done! Let's now take a break...";
 	}
+}
+
+function breakTimer() {
+	countDownText.innerHTML = "Complete!";
+
+	time = breakTime * 60;
+	clearTimeout(timerAction);
+
+	setTimeout(timerStart, 2000);
 }
